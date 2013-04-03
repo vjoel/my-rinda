8e1e4566c590ce799f95179c295ebe494f5ed841
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-04-03 14:40:03 -0700

    added note and example code about pessimistic locking

--- example/optimist.rb
+++ example/optimist.rb
@@ -26,13 +26,22 @@ def client(svr_uri)
   ts = Rinda::TupleSpaceProxy.new(DRbObject.new_with_uri(svr_uri))
   ts.extend Rinda::TupleSpaceProxy::Attempt
   
-  tuple = ts.read [:count, nil]
-  loop do
+  if true
+    tuple = ts.read [:count, nil]
+    loop do
+      count = tuple[1]
+      tuple, entry =
+        ts.attempt [:count, count], [:count, count+1], [:count, nil]
+      break if entry
+      $stderr.puts " "*20 + "pid=#$$ is retrying on #{tuple.inspect}"
+    end
+  else
+    # Alternate implementation using just #take and #write, using
+    # pessimistic locking. Simpler, but more blocked threads. Also,
+    # the tuple cannot be read while a process is working on it.
+    tuple = ts.take [:count, nil]
     count = tuple[1]
-    tuple, entry =
-      ts.attempt [:count, count], [:count, count+1], [:count, nil]
-    break if entry
-    $stderr.puts " "*20 + "pid=#$$ is retrying on #{tuple.inspect}"
+    ts.write [:count, count+1]
   end
   
   $stderr.puts "pid=#$$ incremented #{tuple.inspect}"
